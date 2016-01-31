db7c5da206d06db51caae216ecc97d5d990d75de
oktorakevinarigi <oktoraarigi@ymail.com>
2016-01-31 23:34:10 +0700

    perbaikan login dan daftar

--- FastOrder/app/src/main/java/charlie/fastorder/com/fastorder/Daftar.java
+++ FastOrder/app/src/main/java/charlie/fastorder/com/fastorder/Daftar.java
@@ -1,33 +1,21 @@
 package charlie.fastorder.com.fastorder;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
 
-public class Login extends AppCompatActivity {
-    Button button;
+public class Daftar extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_daftar);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        button = (Button) findViewById(R.id.btndfr);
-        button.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(), Daftar.class);
-                startActivity(i);
-            }
-        });
     }
 
 }
